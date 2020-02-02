d1543ed4a8376716869578ca6e39a098e3e54449
kamykh <fmkaminski@gmail.com>
2020-02-02 18:15:20 +0100

    sskk

--- src/main/java/pl/edu/wszib/controllers/PointOfSalesRestController.java
+++ src/main/java/pl/edu/wszib/controllers/PointOfSalesRestController.java
@@ -37,4 +37,5 @@ public class PointOfSalesRestController {
         reportThatYouCanSeeServiceResponse.setReportThatYouCanSees(reportThatYouCanSees);
         return  reportThatYouCanSeeServiceResponse;
     }
+
 }
--- src/main/java/pl/edu/wszib/controllers/ProductReportController.java
+++ src/main/java/pl/edu/wszib/controllers/ProductReportController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.edu.wszib.SessionObject;
 import pl.edu.wszib.model.RepositoryPointOfSales;
 import pl.edu.wszib.services.IProductReportService;
 
+import javax.annotation.Resource;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,31 +24,49 @@ public class ProductReportController {
     @Autowired
     IProductReportService productReportService;
 
+    @Resource(name = "sessionObject")
+    SessionObject sessionObject;
+
     @RequestMapping(value = "/productReportMenu", method = RequestMethod.GET)
     public String showMenu(){
+        if(this.sessionObject.getUser() == null){
+            return "redirect:loginPage";
+        }
         return "productReportMenu";
     }
 
     @RequestMapping(value = "/showSaleThatDay", method = RequestMethod.POST)
     public String showSTD(@RequestParam(value = "date") String date, Model model) throws ParseException {
+        if(this.sessionObject.getUser() == null){
+            return "redirect:loginPage";
+        }
         model.addAttribute("list", this.productReportService.showWhatSoldThatDay(date));
         return "showSaleThatDay";
     }
 
     @RequestMapping(value = "/showSaleBetween", method = RequestMethod.POST)
     public String showSBD(@RequestParam(value = "date1") String date1, @RequestParam(value = "date2") String date2, Model model) {
+        if(this.sessionObject.getUser() == null){
+            return "redirect:loginPage";
+        }
         model.addAttribute("list", this.productReportService.showWhatSoldBetween(date1,date2));
         return "showSaleBetween";
     }
 
     @RequestMapping(value = "/showIncome", method = RequestMethod.POST)
     public String showIncome(@RequestParam(value = "productName") String productName, Model model) {
+        if(this.sessionObject.getUser() == null){
+            return "redirect:loginPage";
+        }
         model.addAttribute("list", this.productReportService.showIncome(productName));
         return "showIncome";
     }
 
     @RequestMapping(value = "/showStats", method = RequestMethod.POST)
     public String showIncome(Model model) {
+        if(this.sessionObject.getUser() == null){
+            return "redirect:loginPage";
+        }
         model.addAttribute("list", this.productReportService.showStats());
         return "showStats";
     }
